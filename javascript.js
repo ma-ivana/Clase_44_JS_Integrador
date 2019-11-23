7c5f14f8c97a17810e8f11c9d5366f0b1211816c
Ivana <croxcatto.maria.ivana@gmail.com>
2019-11-23 12:46:39 -0300

    Updated code

--- javascript.js
+++ javascript.js
@@ -1,88 +1,199 @@
 let usuarios = [[0, 'Carla', 1545628984, 'carla@gmail.com'], [1, 'Pedro', 1545251245, 'pedro@gmail.com'], [2, 'Lucas', 1523357849, 'lucas@gmail.com'], [3, 'Ana', 15789456, 'ana@gmail.com']];
 
 console.log(usuarios);
+let operacion = "";
+let operacionUpper = "";
+let newArray = [];
+do {
+operacion = prompt(`OPERACIONES POSIBLES
+Elegí la operación que querés realizar:
+----------------------------------------
+  ✚  [CREAR] un usuario
+  🔍  [BUSCAR] un usuario
+  📝  [LISTAR] todos los usuarios
+  ✒️  [MODIFICAR] un usuario
+  ✂  [ELIMINAR] un usuario
+  ✖️  [SALIR] del programa`);
+  operacionUpper = operacion.toUpperCase();
 
 
-let operacion = prompt(`OPERACIONES:
--------------------
-[CREAR] un usuario
-[BUSCAR] un usuario
-[LISTAR] todos los usuarios
-[MODIFICAR] un usuario
-[ELIMINAR] un usuario
-[SALIR] del programa`);
-
-
-switch (operacion) {
+switch (operacionUpper) {
    case "CREAR":
-    let repetir;
-    for (let i=0; repetir !=="NO"; i++) {
+    let repetir = "";
+    let repetirUpper = "";
+    do {
     let nombreUsuarioACrear = prompt("¿Qué usuario querés crear?")
     let telefonoUsuario = prompt("Ingresá el teléfono del usuario")
     let mailUsuario = prompt("Ingresá el mail del usuario")
-    let confirmation = prompt(`Has ingresado los siguientes datos:
+    let confirmation = prompt(`Ingresaste los siguientes datos:
     Nombre: ${nombreUsuarioACrear}
     Teléfono: ${telefonoUsuario}
     Email: ${mailUsuario}
     --------------------------------
-    ¿Desea confirmar esta operación? [SÍ/NO]`)
-    if (confirmation === "SÍ") {
-      let usuarioNuevo = [nombreUsuarioACrear, Number(telefonoUsuario), mailUsuario];
+    ¿Querés confirmar esta operación? [SÍ/NO]`)
+    if (confirmation === "SÍ" || confirmation === "sí" || confirmation === "si" || confirmation === "SI") {
+      let usuarioNuevo = [(usuarios.length -1) + 1, nombreUsuarioACrear, Number(telefonoUsuario), mailUsuario];
       usuarios.push(usuarioNuevo)
       alert(`Operación realizada exitosamente.`)
       console.log(usuarios);
-    } 
+    }
     else {
-      alert(`La operación no se ha realizado.`)
+      alert(`La operación no se realizó.`)
     }
-    repetir = prompt("¿Desea repetir la operación? [SÍ/NO]");
-    if (repetir === "SÍ") {
-    } 
-  }
-  break;
+    repetir = prompt("¿Querés repetir la operación? [SÍ/NO]");
+    repetirUpper = repetir.toUpperCase();
+    } while (repetirUpper !== "NO");
+    break;
 
-  case "BUSCAR":
-  let repetir2;
-    for (let i=0; repetir2 !=="NO"; i++) {
-      let opcionBusqueda = prompt(`Seleccioná una opción de búsqueda:
-      ID, NOMBRE, TELÉFONO, EMAIL`);
-      
-      if (opcionBusqueda === "ID") {
-        datoABuscar = prompt("Ingresá el valor de ID")
-      }
-      else if (opcionBusqueda === "NOMBRE") {
-        datoABuscar = prompt("Ingresá el valor de nombre")
-      }
-      else if (opcionBusqueda === "TELÉFONO") {
-        datoABuscar = prompt("Ingresá el valor de teléfono")
-      }
-      else if (opcionBusqueda === "EMAIL") {
-        datoABuscar = prompt("Ingresá el valor de email")
-      }
-      else {
-        alert("Ingresaste un valor inválido.")
-      }
+    case "BUSCAR":
+      let repetir2 = "";
+      let repetir2Upper = "";
+      do {
 
+        let opcionBusqueda = prompt(`Seleccioná una opción de búsqueda:
+        ID, NOMBRE, TELÉFONO, EMAIL`);
+        switch (opcionBusqueda.toUpperCase()) {
+          case "ID":
+            datoABuscar = prompt("Ingresá el valor de ID");
+            for (let j=0; j<usuarios.length; j++) {
+            for (let m=0; m<usuarios[j].length; m++) {
+            if (usuarios[j][m] == datoABuscar) {
+            alert(`Encontramos el usuario que tiene el dato que buscás:
+            nombre: ${usuarios[j][1]}
+            teléfono: ${usuarios[j][2]}
+            email: ${usuarios[j][3]}`)
+            }
+            }
+            }
+            break;
+          case "NOMBRE":
+            datoABuscar = prompt("Ingresá el valor de nombre");
+            for (let j=0; j<usuarios.length; j++) {
+            for (let m=0; m<usuarios[j].length; m++) {
+            if (usuarios[j][m] == datoABuscar) {
+            alert(`Encontramos el usuario que tiene el dato que buscás:
+            nombre: ${usuarios[j][1]}
+            teléfono: ${usuarios[j][2]}
+            email: ${usuarios[j][3]}`)
+            }
+            }
+            }
+            break;
+          case "TELÉFONO" || "TELEFONO":
+            datoABuscar = prompt("Ingresá el valor de teléfono");
+            for (let j=0; j<usuarios.length; j++) {
+            for (let m=0; m<usuarios[j].length; m++) {
+            if (usuarios[j][m] == datoABuscar) {
+            alert(`Encontramos el usuario que tiene el dato que buscás:
+            nombre: ${usuarios[j][1]}
+            teléfono: ${usuarios[j][2]}
+            email: ${usuarios[j][3]}`)
+            }
+            }
+            }
+            break;
+          case "EMAIL":
+            datoABuscar = prompt("Ingresá el valor de email");
+            for (let j=0; j<usuarios.length; j++) {
+            for (let m=0; m<usuarios[j].length; m++) {
+            if (usuarios[j][m] == datoABuscar) {
+            alert(`Encontramos el usuario que tiene el dato que buscás:
+            nombre: ${usuarios[j][1]}
+            teléfono: ${usuarios[j][2]}
+            email: ${usuarios[j][3]}`)
+            }
+            }
+            }
+            break;
+          default:
+            alert("Ingresaste un valor inválido.");
+            break;
+      }
+        repetir2 = prompt("¿Querés repetir la operación? [SÍ/NO]");
+        repetir2Upper = repetir2.toUpperCase();
+      } while (repetir2Upper !== "NO");
+      break;
 
-      for (let j=0; j<usuarios.length; j++) {
-        for (let m=0; m<usuarios[j].length; m++) {
-          if (usuarios[j][m] == datoABuscar) {
-            alert(`Encontramos el usuario ${usuarios[j]} que tiene el dato que buscás`)
-          }
+    case "LISTAR":
+      let string = "";
+      for (let i=0; i<usuarios.length; i++) {
+      string += ` 
+                  nombre: ${usuarios[i][1]}
+                  teléfono: ${usuarios[i][2]}
+                  email: ${usuarios[i][3]}
+                  ----------------------------------`   
+      }
+      alert(string);
+      break;
+
+    case "MODIFICAR":
+      let repetir3 = "";
+      let repetir3Upper = "";
+      do {
+        let idParaModificar = prompt(`Ingresá el ID del registro que querés modificar:`);
+        let nuevoNombre = prompt(`Ingresá el nuevo nombre:`)
+        let nuevoTelefono = prompt(`Ingresá el nuevo teléfono:`)
+        let nuevoEmail = prompt (`Ingresá el nuevo email:`)
+        
+        usuarios[Number(idParaModificar)].splice(0, 4, Number(idParaModificar), nuevoNombre, nuevoTelefono, nuevoEmail);
+        
+        alert(`Registro cambiado:
+        ID: ${Number(idParaModificar)}
+        nombre: ${nuevoNombre}
+        teléfono: ${nuevoTelefono}
+        email: ${nuevoEmail}`)
+        repetir3 = prompt("¿Querés repetir la operación? [SÍ/NO]");
+        repetir3Upper = repetir3.toUpperCase();
+      } while (repetir3Upper !== "NO");
+      break;
+
+    case "ELIMINAR":
+      let repetir4 = "";
+      let repetir4Upper = "";
+      do {
+        let idParaEliminar = prompt(`Ingresá el ID del registro que querés eliminar:`);
+        let idEnNro = Number(idParaEliminar);
+        let confirmation2 = prompt(`Se eliminará el siguiente registro:
+        ID: ${idEnNro}
+        nombre: ${usuarios[idEnNro][1]}
+        teléfono: ${usuarios[idEnNro][2]}
+        email: ${usuarios[idEnNro][3]}
+    --------------------------------
+    ¿Querés confirmar esta operación? [SÍ/NO]`)
+    if (confirmation2 === "SÍ" || confirmation2 === "sí" || confirmation2 === "si" || confirmation2 === "SI") {
+      usuarios[idEnNro].splice(0, 4);
+      usuarios[idEnNro] = null;
+      alert(`Operación realizada exitosamente.`)
+   
+      console.log(usuarios);
+
+      for (let i=0; i<usuarios.length; i++) {
+        if (usuarios[i] !== null) {
+          newArray.push(usuarios[i])
         }
-
       }
-
-    
-    repetir2 = prompt("¿Desea repetir la operación? [SÍ/NO]");
-    if (repetir2 === "SÍ") {
+      console.log(newArray);
     }
+    else {
+      alert(`La operación no se realizó.`)
+    }
+
+    repetir4 = prompt("¿Querés repetir la operación? [SÍ/NO]");
+    repetir4Upper = repetir4.toUpperCase();
+      } while (repetir4Upper !== "NO");
+      break;
+
+    default:
+      break;
   }
-  break;
+} while (operacionUpper !== "SALIR");
 
-  default:
-  break;
-
-}
+alert("¡Hasta pronto! ツ")
 
 
+
+
+
+
+//https://github.com/Ada-IT/bootcamp-frontend/blob/master/integradores/Integrador%202.%20Estructuras%20de%20control.md
+//https://malerey.github.io/Integrador-2/index.html 
